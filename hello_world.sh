304ad06ca2f5c71fb2d191e47efb8f781d7e33b7
tomocy <tomocy.dev@gmail.com>
2020-03-09 23:11:38 +0900

    update hello world script
    This script shows the address of the deployment and the command to delete it.

--- hello_world.sh
+++ hello_world.sh
@@ -8,3 +8,12 @@ echo 'expose hello world deployment'
 kubectl expose deployment hello-node \
 --type LoadBalancer \
 --port 8080
+
+IP=$(kubectl get service hello-node \
+-o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+echo 'The deployment of hello-node can be accessed by the following address'
+echo ' http://${IP}:8080'
+
+echo 'delete the deployment if you do not want to be charged by the following command'
+echo ' kubectl delete service hello-node'
+echo ' kubectl delete deployment hello-node'
