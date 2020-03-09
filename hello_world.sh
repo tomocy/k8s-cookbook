b7cdb01ac0fc278941dc2b5b2ee74f0c21448740
tomocy <tomocy.dev@gmail.com>
2020-03-09 23:30:31 +0900

    fix

--- hello_world.sh
+++ hello_world.sh
@@ -9,11 +9,20 @@ kubectl expose deployment hello-node \
 --type LoadBalancer \
 --port 8080
 
+echo 'get the address of hello world deployment'
+while true
+do
 IP=$(kubectl get service hello-node \
 -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+if [ ! -z "${IP}" ]
+then
+break
+fi
+sleep 1
+done
 echo 'The deployment of hello-node can be accessed by the following address'
-echo ' http://${IP}:8080'
+echo "  http://${IP}:8080"
 
 echo 'delete the deployment if you do not want to be charged by the following command'
-echo ' kubectl delete service hello-node'
-echo ' kubectl delete deployment hello-node'
+echo '  kubectl delete service hello-node'
+echo '  kubectl delete deployment hello-node'
